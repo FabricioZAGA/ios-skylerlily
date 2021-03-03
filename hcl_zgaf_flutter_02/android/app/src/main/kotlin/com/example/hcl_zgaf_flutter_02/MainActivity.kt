662e4f217fb3a0f07e3f514b123db4d5ad4f0dac
Fabricio ZAGA <fabricio@wikoud.com>
2021-03-03 16:34:32 -0600

    add: second practice flutter app

---
+++ hcl_zgaf_flutter_02/android/app/src/main/kotlin/com/example/hcl_zgaf_flutter_02/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hcl_zgaf_flutter_02
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hcl_zgaf_flutter_02/lib/src/pages/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Aqui va el titulo'),
+      ),
+    );
+  }
+}
